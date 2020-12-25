bfa69182d6ba7dea74b75b687791418b90041f5b
parkernilson <parker.todd.nilson@gmail.com>
2020-12-25 13:29:15 -0700

    set static maxage to 1h

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const staticOptions = {
   etag: false,
   extensions: ['htm', 'html'],
   index: false,
-  maxAge: '1d',
+  maxAge: '1h',
   redirect: false,
   setHeaders: function (res, path, stat) {
     res.set('x-timestamp', Date.now())
